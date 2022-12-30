58b1e942913c77477cddf476ecaa13a3fa1985de
filip19992 <filipwlodarczyk99@gmail.com>
2022-12-30 03:36:54 +0100

    adjusted to .net platform

---
+++ demo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace demo_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("returns")]
+        public ActionResult Returns()
+        {
+            return View();
+        }
+
+        [Route("shoppings")]
+        public ActionResult Shoppings()
+        {
+            return View();
+        }
+
+        [Route("shop-rules")]
+        public ActionResult ShopRules()
+        {
+            return View();
+        }
+    }
+}
