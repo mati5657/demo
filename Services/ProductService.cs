20ba12c73b02ee0d95f8653224ae6d7c194f252a
filip19992 <filipwlodarczyk99@gmail.com>
2023-01-05 23:07:00 +0100

    Added produtService.cs

---
+++ Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using Pugcorn_v1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pugcorn_v1
+{
+    public class ProductService
+    {
+        private readonly DbPugcornContext _context;
+
+        public ProductService(DbPugcornContext context)
+        {
+            _context = context;
+        }
+        public int Save(Product product)
+        {
+            _context.Add(product);
+            return 1;
+        }
+
+        public List<Product> FindAll()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
