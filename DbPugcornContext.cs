299352c2cc43b743f8cc696ad329fc29be44aafa
filip19992 <filipwlodarczyk99@gmail.com>
2023-01-05 22:02:44 +0100

    Added database configuration

---
+++ DbPugcornContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pugcorn_v1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pugcorn_v1
+{
+    public class DbPugcornContext : DbContext
+    {
+        public DbPugcornContext(DbContextOptions<DbPugcornContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -35,6 +36,11 @@ namespace Pugcorn_v1
             services.AddAuthentication();
             services.AddRazorPages()
                  .AddMicrosoftIdentityUI();
+
+            services.AddDbContext<DbPugcornContext>(builder =>
+            {
+                builder.UseSqlServer("Data Source=DESKTOP-70TA2IQ;Initial Catalog=PugcornDb;Integrated Security=True") ;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
